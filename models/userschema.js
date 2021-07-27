afdcef7ed26325685f9614636704c6302a538fff
Pranav <pranavbalijapelly@gmail.com>
2021-07-27 18:39:34 +0530

    updated userschema and added teamup routes

--- models/userschema.js
+++ models/userschema.js
@@ -1,31 +1,41 @@
-const mongoose = require('mongoose')
-const userschema= new mongoose.Schema({
-    Name:String,
-    Mail_Id:String,
-    user_profile:String,
-    Year:Number,
-    Branch:String,
-    Myposts:[{
-        Post_id:mongoose.Schema.Types.ObjectId,
-        Type:String
-    }],
-    Starred:[{
-        Post_id:mongoose.Schema.Types.ObjectId,
-        Type:String
-    }],
-    ReceivedRequests:[{
-        Post_id:mongoose.Schema.Types.ObjectId,
-        Name:String,
-        Description:String,
-        RequestedUser_id:mongoose.Schema.Types.ObjectId
-    }],
-    Myrequests:[{
-        Post_id:mongoose.Schema.Types.ObjectId,
-        Description:String,
-        Status: Boolean
-    }]
-
-})
-const User = mongoose.model('user',userschema);
+const mongoose = require("mongoose");
+const userschema = new mongoose.Schema({
+  Name: String,
+  Mail_Id: String,
+  user_profile: String,
+  Year: Number,
+  Branch: String,
+  Myposts: [
+    {
+      Post_id: mongoose.Schema.Types.ObjectId,
+      Type: String,
+    },
+  ],
+  Starred: [
+    {
+      Post_id: mongoose.Schema.Types.ObjectId,
+      Type: String,
+    },
+  ],
+  ReceivedRequests: [
+    {
+      Post_id: mongoose.Schema.Types.ObjectId,
+      Name: String,
+      Description: String,
+      MyDescription:String,
+      RequestedUser_id: mongoose.Schema.Types.ObjectId,
+      Status: Number
+    },
+  ],
+  Myrequests: [
+    {
+      Post_id: mongoose.Schema.Types.ObjectId,
+      Description: String,
+      ReceivedDescription: String,
+      Status: Number,
+    },
+  ],
+});
+const User = mongoose.model("UserData", userschema);
 
 module.exports = User;
