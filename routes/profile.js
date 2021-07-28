a5cb9c359c2d1a83b107c54e6e65e0e23278bd56
Pranav <pranavbalijapelly@gmail.com>
2021-07-28 19:50:57 +0530

    added delete feature

--- routes/profile.js
+++ routes/profile.js
@@ -17,7 +17,7 @@ const ensureAuth = (req, res, next) => {
 router.get("/myposts", ensureAuth, (req, res) => {
   async function getdata() {
     const mypostdata = req.user.Myposts;
-    console.log(mypostdata);
+    // console.log(mypostdata);
     let response = [];
     for (let i = 0; i < mypostdata.length; i++) {
       const data = await PostsSchema.findOne({
@@ -31,8 +31,12 @@ router.get("/myposts", ensureAuth, (req, res) => {
 });
 
 router.post("/interested", ensureAuth, (req, res) => {
-  console.log(req.body);
+//   console.log(req.body);
   async function saving() {
+
+    const user_data = await PostsSchema.findOne({
+        _id: mongoose.Types.ObjectId(req.body.post_mong_id),
+      });
     const saveddata = await User.updateOne(
       { _id: req.user.id },
       {
@@ -41,15 +45,17 @@ router.post("/interested", ensureAuth, (req, res) => {
             Post_id: mongoose.Types.ObjectId(req.body.post_mong_id),
             Description: req.body.AlertDescription,
             ReceivedDescription:"-",
+            PostOwner: user_data.Name,
+            PostTitle: user_data.Requirements,
             Status: 1,
           },
         },
       }
     );
-
-    const user_data = await PostsSchema.findOne({
-      _id: mongoose.Types.ObjectId(req.body.post_mong_id),
-    });
+    console.log("noel kosam",saveddata);
+    // const user_data = await PostsSchema.findOne({
+    //   _id: mongoose.Types.ObjectId(req.body.post_mong_id),
+    // });
     const UserId = user_data.User_Id;
     const updatedata = await User.updateOne(
       { _id: UserId },
@@ -61,6 +67,7 @@ router.post("/interested", ensureAuth, (req, res) => {
             MyDescription:"-",
             Name: req.user.Name,
             RequestedUser_id: req.user.id,
+            PostTitle:user_data.Requirements,
             Status:1
           },
         },
@@ -85,12 +92,16 @@ router.get('/myrequests',ensureAuth,(req,res)=>{
             // console.log(result)
             const dummy={
                 post:result, 
-                Description:receivedrequests[i].Description, 
-                status:receivedrequests[i].Status
+                Description:receivedrequests[i].Description,
+                MeetDescription:receivedrequests[i].ReceivedDescription,
+                status:receivedrequests[i].Status,
+                PostOwner:receivedrequests[i].PostOwner,
+                PostTitle:receivedrequests[i].PostTitle,
+                id:receivedrequests[i]._id
             }
             datatobesent.push(dummy);
         }
-        console.log(datatobesent)
+        // console.log(datatobesent)
         res.send(datatobesent);
     }
     get_data();
@@ -107,19 +118,20 @@ router.get('/receivedrequests',ensureAuth,(req,res)=>{
             })
             // console.log(result)
             const dummy={
-                post:result, Description:receivedrequests[i].Description, 
+                post:result,
+                Description:receivedrequests[i].Description, 
                 requestedUser_id:receivedrequests[i].RequestedUser_id, 
+                MyDescription:receivedrequests[i].MyDescription,
                 name:receivedrequests[i].Name,
-                status:receivedrequests[i].status
+                status:receivedrequests[i].status,
+                PostTitle:receivedrequests[i].PostTitle
             }
             datatobesent.push(dummy);
         }
-        console.log(datatobesent)
+        // console.log(datatobesent)
         res.send(datatobesent);
     }
     get_data();
-
-
 });
 
 
@@ -140,7 +152,7 @@ router.post("/confirmrequest",ensureAuth,(req,res)=>{
                     }
                 }
             )
-        console.log("Received request result",result);
+        // console.log("Received request result",result);
     } 
     updatedata();
     async function updatedata2 (){
@@ -153,10 +165,59 @@ router.post("/confirmrequest",ensureAuth,(req,res)=>{
                     }
                 }
             )
-        console.log("Myrequest result",result);
+        // console.log("Myrequest result",result);
     } 
     updatedata2();
     res.sendStatus(200);
+})
+
+
+router.post("/myposts/delete",ensureAuth,(req,res)=>{
+    const datatobedeleted= toString(req.body.postid);
+    console.log("delete data mongo id",datatobedeleted)
+    const array=req.user.ReceivedRequests;
+    const tobedeletedarray=[]
+    for(let i=0;i<array.length;i++){
+ 
+      if(toString(array[i].Post_id)==datatobedeleted){
+          tobedeletedarray.push(array[i].RequestedUser_id);
+      }
+    }
+    console.log(tobedeletedarray);
+    for(let i=0;i<tobedeletedarray.length;i++){
+        async function updatedata2 (){
+            const result = await User.updateOne(
+                { _id: tobedeletedarray[i], "Myrequests.Post_id":mongoose.Types.ObjectId(req.body.postid) },
+                {
+                    $set: {
+                        "Myrequests.$.Post_id":null
+                    }
+                }
+            )
+        } 
+        updatedata2();
+    }
+    async function mypostdelete (){
+        const result = await User.updateOne({ _id: req.user.id }, { "$pull": { "Myposts": { "Post_id": mongoose.Types.ObjectId(req.body.postid) } }} );
+        console.log(result);
+    } 
+    mypostdelete();
+    console.log( "post id ",mongoose.Types.ObjectId(req.body.postid))
+    async function postdelete (){
+        const result = await PostsSchema.remove({ _id:  mongoose.Types.ObjectId(req.body.postid)});
+        console.log(result);
+    } 
+    postdelete();
+    console.log( "post id ",mongoose.Types.ObjectId(req.body.postid))
+
+    async function mypostdelete1 (){
+        const result = await User.updateOne({ _id: req.user.id }, { "$pull": { "ReceivedRequests": { "Post_id": mongoose.Types.ObjectId(req.body.postid) , Status: {"$ne": 2}} }} );
+        console.log(result);
+    } 
+    mypostdelete1();
+
+
+
 })
 
 module.exports = router;
